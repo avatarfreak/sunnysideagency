5045feae559e652eeccaec2297eb5bc60b1f6507
avatarfreak <avatarfreak83@gmail.com>
2021-06-25 19:31:07 +0530

    added function to close menu on clicking outside

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 const menuToggler = document.querySelector(".navbar__toggler img");
 const nav = document.querySelector(".collapsible-nav");
 
-menuToggler.addEventListener("click", () => {
+menuToggler.addEventListener("click", function (evt) {
+  //decrease opacity of the hamburger or vice-versa
+  this.classList.toggle("active");
+  //show dropdown menu or vice-versa
   nav.classList.toggle("active");
+});
+
+//Close dropdown menu if clicked outside  navigation or hamburger
+document.addEventListener("click", (evt) => {
+  if (evt.target == menuToggler || nav.contains(evt.target)) {
+    return;
+  }
+  nav.classList.remove("active");
+  menuToggler.classList.remove("active");
 });
